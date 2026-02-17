9e73a34f5133fbf314743049624a99e0302abd0c
Ashutosh556087 <25110cn065@glbitm.ac.in>
2026-02-17 11:16:18 +0530

    Add files via upload

---
+++ ashutosh singh.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main(){
+    int x ;
+    scanf("%d",&x);
+    if(x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u'){
+        printf("it is a vovel /n");
+    }else{
+        printf("it isnot vovel ");
+    }return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
