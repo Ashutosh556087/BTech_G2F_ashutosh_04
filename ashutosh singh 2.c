eca2c4be514b89362c71cdc1bf16c69ca7e79f4c
Ashutosh556087 <25110cn065@glbitm.ac.in>
2026-02-17 11:45:55 +0530

    Add files via upload

---
+++ ashutosh singh 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+     int x ; 
+    printf("enter a number");
+    scanf("%d", &x);
+    if(x>0){
+        printf("it is a positive number \n");
+    }else{
+        printf("it is a negative  number \n");
+        
+    }
+return 0 ;
+}
+
+
+
+
+
+
+
+
+
+
+
