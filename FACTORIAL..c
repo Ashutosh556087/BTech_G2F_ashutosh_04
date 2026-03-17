3125c031d548f5d40144c934b550be9f82265d12
Ashutosh556087 <25110cn065@glbitm.ac.in>
2026-03-17 10:10:51 +0530

    Add files via upload

--- FACTORIAL..c
+++ FACTORIAL..c
@@ -1,13 +1,22 @@
-#include<stdio.h>
-int main()
-{
-    int n, factorial=1;
-    printf("Enter the no. to find factorial:");
-    scanf("%d",&n);
-    for (int i=1;i<=n;i++)
-    {
-        factorial = factorial*i;
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Prompt the user to enter an integer
+    printf("Enter an integer: ");
+    
+    // Read the input from the user
+    scanf("%d", &n);
+
+    // Print the header for the table
+    printf("Multiplication table of %d:\n", n);
+    printf("--------------------------\n");
+
+    // Loop from 1 to 10 to generate the table
+    for (i = 1; i <= 10; ++i) {
+        printf("%d * %d = %d \n", n, i, n * i);
     }
-    printf("factorial of given no. is :%d", factorial);
+
     return 0;
 }
